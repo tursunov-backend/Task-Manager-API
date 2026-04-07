d9488378e71574cbe8a8648e8ec9e209c51e1d2c
tursunov-backend <donitursunov98@gmail.com>
2026-04-07 22:02:40 +0500

    app database

---
+++ app/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import URL, create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from .config import settings
+
+
+url = URL.create(
+    drivername="postgresql+psycopg2",
+    host=settings.db_host,
+    port=settings.db_port,
+    username=settings.db_user,
+    password=settings.db_pass,
+    database=settings.db_name
+)
+
+engine = create_engine(url)
+SessionLocal = sessionmaker(engine)
+
+
+class Base(DeclarativeBase):
+    pass
