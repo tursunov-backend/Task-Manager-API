b9f7bd9ff81b02348c1710cd694a9b47187743a5
tursunov-backend <donitursunov98@gmail.com>
2026-04-08 14:35:14 +0500

    project models

--- app/models/project.py
+++ app/models/project.py
@@ -1,26 +1,26 @@
-# from datetime import datetime
-# from typing import List
+from datetime import datetime
+from typing import List
 
-# from sqlalchemy.orm import Mapped, mapped_column, relationship
-# from sqlalchemy import String, ForeignKey, DateTime, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey, DateTime, Text
 
-# from app.database import Base
+from app.database import Base
 
 
-# class Project(Base):
-#     __tablename__ = "projects"
+class Project(Base):
+    __tablename__ = "projects"
 
-#     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
-#     title: Mapped[str] = mapped_column(String(255), nullable=False)
-#     description: Mapped[str] = mapped_column(Text, nullable=True)
-#     author_id: Mapped[int] = mapped_column(
-#         ForeignKey("users.id", ondelete="CASCADE"), nullable=False
-#     )
-#     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    author_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
-#     users: Mapped[List["User"]] = relationship(
-#         "User", uselist=True, back_populates="projects"
-#     )
+    users: Mapped[List["User"]] = relationship(
+        "User", uselist=True, back_populates="projects"
+    )
 
-#     def __str__(self):
-#         return f"{self.id}. {self.title}"
+    def __str__(self):
+        return f"{self.id}. {self.title}"
