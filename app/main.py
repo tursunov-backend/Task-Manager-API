f78b3921f8d8094c6b8e0e3e45c7e53daa22a374
tursunov-backend <donitursunov98@gmail.com>
2026-04-09 16:59:24 +0500

    main

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
-# from fastapi import FastAPI
+from fastapi import FastAPI
 
-# from app.database import engine, Base
+from app.database import engine, Base
 
-# from app.routers import auth, users, projects, tasks
-
-
-# app = FastAPI(
-#     title="Task Manager API",
-#     version="1.0.0",
-# )
-
-# Base.metadata.create_all(bind=engine)
+from app.routers import auth, users, projects, tasks
 
 
-# app.include_router(auth.router, prefix="/api")
-# app.include_router(users.router, prefix="/api")
-# app.include_router(projects.router, prefix="/api")
-# app.include_router(tasks.router, prefix="/api")
+app = FastAPI(
+    title="Task Manager API",
+    version="1.0.0",
+)
+
+Base.metadata.create_all(bind=engine)
 
 
-# @app.get("/")
-# def root():
-#     return {"message": "API working 🚀"}
+app.include_router(auth.router, prefix="/api")
+app.include_router(users.router, prefix="/api")
+app.include_router(projects.router, prefix="/api")
+app.include_router(tasks.router, prefix="/api")
+
+
+@app.get("/")
+def root():
+    return {"message": "API working 🚀"}
