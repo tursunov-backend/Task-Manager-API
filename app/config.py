dd1454ba6da6345a7dd33c3a6ad5a41a93d817d1
tursunov-backent <donitursunov98@gmail.com>
2026-04-15 22:25:04 +0500

    config

--- app/config.py
+++ app/config.py
@@ -10,8 +10,8 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
